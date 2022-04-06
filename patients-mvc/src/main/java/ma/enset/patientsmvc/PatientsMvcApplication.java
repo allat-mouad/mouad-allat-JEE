dcd0588f2c7f8be4bde9926da89e97af2c528e75
allat mouad <80215500+moallat@users.noreply.github.com>
2022-04-06 23:55:28 +0000

    UserDetailsService authentification

--- patients-mvc/src/main/java/ma/enset/patientsmvc/PatientsMvcApplication.java
+++ patients-mvc/src/main/java/ma/enset/patientsmvc/PatientsMvcApplication.java
@@ -2,6 +2,7 @@ package ma.enset.patientsmvc;
 
 import ma.enset.patientsmvc.entities.Patient;
 import ma.enset.patientsmvc.repositories.PatientRepository;
+import ma.enset.patientsmvc.security.service.SecurityService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -33,4 +34,21 @@ public class PatientsMvcApplication {
             }
     };
     }
+
+    //@Bean
+    CommandLineRunner start(SecurityService securityService){
+        return args -> {
+            securityService.saveNewUser("mouad","1234","1234");
+            securityService.saveNewUser("tomas","1234","1234");
+            securityService.saveNewUser("karim","1234","1234");
+            securityService.saveNewRole("USER","");
+            securityService.saveNewRole("ADMIN","");
+
+            securityService.AddRoleToUser("mouad","ADMIN");
+            securityService.AddRoleToUser("mouad","USER");
+            securityService.AddRoleToUser("tomas","USER");
+            securityService.AddRoleToUser("karim","USER");
+
+        };
+}
 }
--- patients-mvc/src/main/java/ma/enset/patientsmvc/security/SecurityConfig.java
+++ patients-mvc/src/main/java/ma/enset/patientsmvc/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package ma.enset.patientsmvc.security;
 
+import ma.enset.patientsmvc.security.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -18,12 +22,14 @@ import javax.sql.DataSource;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private DataSource dataSource;
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     //on precise comment spring security va cercher les users et les roles
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-        PasswordEncoder passwordEncoder=passwordEncoder();
+        PasswordEncoder passwordEncoder=mypasswordEncoder();
 
         /*
         //inMemoryAuthentication est tres pratique pour tester les applications
@@ -32,27 +38,42 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication().withUser("user2").password(passwordEncoder.encode("2345")).roles("USER");
 
         auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder.encode("1111")).roles("USER","ADMIN");
-       */
-        auth.jdbcAuthentication()
+
+
+ auth.userDetailsService(new UserDetailsService() {
+           @Override
+           public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+               return null;
+           }
+       });
+
+       auth.jdbcAuthentication()
                 .dataSource(dataSource)
                 .usersByUsernameQuery("SELECT username as principal,password as credentials,active FROM users WHERE username=?")
                 .authoritiesByUsernameQuery("SELECT username as principal,role as role FROM  user_role WHERE username=?")
                 .rolePrefix("ROLE_")
                 .passwordEncoder(passwordEncoder);
+
+       */
+        //user detail service permet a la couche service de d'occuper des l'authentification
+
+        auth.userDetailsService(userDetailsService);
+
         }
 
     @Override//pour spicifier les droits d'acces
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin();
         http.authorizeHttpRequests().antMatchers("/").permitAll();
-        http.authorizeHttpRequests().antMatchers("/admin/**").hasRole("ADMIN");
-        http.authorizeHttpRequests().antMatchers("/user/**").hasAnyRole("USER");
+        http.authorizeHttpRequests().antMatchers("/admin/**").hasAuthority("ADMIN");
+        http.authorizeHttpRequests().antMatchers("/user/**").hasAuthority("USER");
+        http.authorizeHttpRequests().antMatchers("/webjars/**").permitAll();
         http.authorizeHttpRequests().anyRequest().authenticated();//tous les requetes nessecite une authentification
         http.exceptionHandling().accessDeniedPage("/403");
 
     }
     @Bean //au demarage cree moi un objet PasswordEncoder et place dans ton contexte
-    PasswordEncoder passwordEncoder(){
+    PasswordEncoder mypasswordEncoder(){
         return  new BCryptPasswordEncoder();
     }
 
