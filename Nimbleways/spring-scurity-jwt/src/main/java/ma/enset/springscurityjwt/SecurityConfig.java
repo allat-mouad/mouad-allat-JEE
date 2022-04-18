155a547b87f8e95f3b24e3430aff256692930bd6
allat mouad <80215500+moallat@users.noreply.github.com>
2022-04-18 02:08:48 +0000

    fixing bugs

---
+++ Nimbleways/spring-scurity-jwt/src/main/java/ma/enset/springscurityjwt/SecurityConfig.java
@@ -0,0 +1,6 @@
+package ma.enset.springscurityjwt;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}
--- etudiants-mvc/src/main/java/ma/enset/etudiantsmvc/entities/Etudiant.java
+++ etudiants-mvc/src/main/java/ma/enset/etudiantsmvc/entities/Etudiant.java
@@ -7,6 +7,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 import java.util.Date;
@@ -23,7 +24,8 @@ public class Etudiant {
     @NotEmpty
     @Size(min=4,max=70)
     private String nom;
-    @Size(min=4,max=70)
+    @Email(message = "Email is not valid", regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
+    @NotEmpty(message = "Email cannot be empty")
     private String email;
     @Enumerated(EnumType.STRING)
     private Genre genre;
